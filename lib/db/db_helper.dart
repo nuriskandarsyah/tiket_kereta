011e04a46e7a74b5cf9dd0c1439dacd2ed51136c
nuriskandarsyah <nuriskandarsyah010@gmail.com>
2024-12-17 11:32:45 +0700

    initialized

--- lib/db/db_helper.dart
+++ lib/db/db_helper.dart
@@ -1,21 +1,26 @@
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
-class DBHelper {
-  static final DBHelper instance = DBHelper._init();
+class DatabaseHelper {
+  static final DatabaseHelper instance = DatabaseHelper._init();
   static Database? _database;
 
-  DBHelper._init();
+  DatabaseHelper._init();
 
+  // Singleton Instance
+  factory DatabaseHelper() => instance;
+
+  // Get Database Instance
   Future<Database> get database async {
     if (_database != null) return _database!;
-    _database = await _initDB('flutter_crud.db');
+    _database = await _initDB('sewa_buku.db');
     return _database!;
   }
 
-  Future<Database> _initDB(String fileName) async {
+  // Initialize Database
+  Future<Database> _initDB(String filePath) async {
     final dbPath = await getDatabasesPath();
-    final path = join(dbPath, fileName);
+    final path = join(dbPath, filePath);
 
     return await openDatabase(
       path,
@@ -24,71 +29,53 @@ class DBHelper {
     );
   }
 
+  // Create Table
   Future _createDB(Database db, int version) async {
     await db.execute('''
-      CREATE TABLE users (
+      CREATE TABLE sewa_buku (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        username TEXT NOT NULL,
-        password TEXT NOT NULL
+        nama TEXT NOT NULL,
+        alamat TEXT NOT NULL,
+        nama_buku Text NOT NULL,
+        tanggal_sewa TEXT NOT NULL,
+        tanggal_kembali TEXT NOT NULL,
+        total_bayar INTEGER NOT NULL
       )
     ''');
   }
 
-  Future close() async {
+  // Add Data
+  Future<int> addSewa(Map<String, dynamic> data) async {
     final db = await instance.database;
-    db.close();
+    return await db.insert('sewa_buku', data);
   }
 
-Future<void> addUser(String username, String password) async {
-  final db = await instance.database;
+  // Get All Data (Ordered by ID Ascending)
+  Future<List<Map<String, dynamic>>> getAllSewa() async {
+    final db = await instance.database;
+    return await db.query('sewa_buku', orderBy: 'id ASC');
+  }
 
-  await db.insert(
-    'users',
-    {'username': username, 'password': password},
-    conflictAlgorithm: ConflictAlgorithm.replace,
-  );
+  // Update Data
+  Future<int> updateSewa(Map<String, dynamic> data) async {
+    final db = await instance.database;
+    return await db.update(
+      'sewa_buku',
+      data,
+      where: 'id = ?',
+      whereArgs: [data['id']],
+    );
+  }
+
+  // Delete Data
+  Future<int> deleteSewa(int id) async {
+    final db = await instance.database;
+    return await db.delete('sewa_buku', where: 'id = ?', whereArgs: [id]);
+  }
+
+  Future<void> vacuumDatabase() async {
+    final db = await instance.database;
+    await db.execute('VACUUM');
+    print("Database telah dioptimalkan dengan VACUUM.");
+  }
 }
-
-  Future<List<Map<String, dynamic>>> getUsers() async {
-  final db = await instance.database;
-
-  return await db.query('users');
-}
-
-Future<void> updateUser(int id, String username, String password) async {
-  final db = await instance.database;
-
-  await db.update(
-    'users',
-    {'username': username, 'password': password},  // Update username dan password
-    where: 'id = ?',
-    whereArgs: [id],
-  );
-}
-
-
-Future<void> deleteUser(int id) async {
-  final db = await instance.database;
-
-  await db.delete(
-    'users',
-    where: 'id = ?',
-    whereArgs: [id],
-  );
-}
-
-Future<bool> login(String username, String password) async {
-  final db = await instance.database;
-
-  final result = await db.query(
-    'users',
-    where: 'username = ? AND password = ?',
-    whereArgs: [username, password],
-  );
-
-  return result.isNotEmpty;
-}
-
-}
-
-
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,277 @@
 import 'package:flutter/material.dart';
-import 'screens/login_screen.dart';
-import 'screens/user_list_screen.dart';
+import 'package:intl/intl.dart';
+import 'db/db_helper.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
-      routes: {
-        '/home': (context) => UserListScreen(),
+      home: SewaBukuPage(),
+    );
+  }
+}
+
+class SewaBukuPage extends StatefulWidget {
+  @override
+  _SewaBukuPageState createState() => _SewaBukuPageState();
+}
+
+class _SewaBukuPageState extends State<SewaBukuPage> {
+  final dbHelper = DatabaseHelper.instance;
+
+  final _namaController = TextEditingController();
+  final _alamatController = TextEditingController();
+  final _namaBukuController = TextEditingController();
+  final _tanggalSewaController = TextEditingController();
+  final _tanggalKembaliController = TextEditingController();
+
+  final int _hargaPerHari = 10000;
+
+  List<Map<String, dynamic>> _notes = [];
+  Map<String, dynamic>? _selectedItem;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadNotes();
+  }
+
+  _loadNotes() async {
+    final notes = await dbHelper.getAllSewa();
+    setState(() {
+      _notes = notes;
+    });
+  }
+
+  Future<void> _saveData() async {
+    if (_namaController.text.isEmpty ||
+        _alamatController.text.isEmpty ||
+        _namaBukuController.text.isEmpty ||
+        _tanggalSewaController.text.isEmpty ||
+        _tanggalKembaliController.text.isEmpty) {
+      _showSnackbar("Harap isi semua kolom!");
+      return;
+    }
+
+    final tanggalSewa = DateTime.parse(_tanggalSewaController.text);
+    final tanggalKembali = DateTime.parse(_tanggalKembaliController.text);
+
+    if (tanggalKembali.isBefore(tanggalSewa)) {
+      _showSnackbar("Tanggal Kembali harus setelah Tanggal Sewa!");
+      return;
+    }
+
+    int lamaSewa = tanggalKembali.difference(tanggalSewa).inDays;
+    lamaSewa = lamaSewa == 0 ? 1 : lamaSewa;
+    print("lama Sewa: $lamaSewa hari");
+    final totalBayar = lamaSewa * _hargaPerHari;
+
+    if (_selectedItem == null) {
+      // Tambah Data Baru
+      await dbHelper.addSewa({
+        'nama': _namaController.text,
+        'alamat': _alamatController.text,
+        'nama_buku': _namaBukuController.text,
+        'tanggal_sewa': _tanggalSewaController.text,
+        'tanggal_kembali': _tanggalKembaliController.text,
+        'total_bayar': totalBayar,
+      });
+    } else {
+      // Edit Data
+      await dbHelper.updateSewa({
+        'id': _selectedItem!['id'],
+        'nama': _namaController.text,
+        'alamat': _alamatController.text,
+        'nama_buku': _namaBukuController.text,
+        'tanggal_sewa': _tanggalSewaController.text,
+        'tanggal_kembali': _tanggalKembaliController.text,
+        'total_bayar': totalBayar,
+      });
+      _selectedItem = null;
+    }
+
+    _clearInput();
+    _loadNotes();
+    _showSnackbar("Data berhasil disimpan!");
+  }
+
+  void _clearInput() {
+    _namaController.clear();
+    _alamatController.clear();
+    _namaBukuController.clear();
+    _tanggalSewaController.clear();
+    _tanggalKembaliController.clear();
+    _selectedItem = null;
+  }
+
+  void _showSnackbar(String message) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text(message),
+      duration: Duration(seconds: 2),
+    ));
+  }
+
+  Widget _buildTextField(TextEditingController controller, String label) {
+    return TextField(
+      controller: controller,
+      decoration: InputDecoration(
+        labelText: label,
+        border: OutlineInputBorder(),
+      ),
+    );
+  }
+
+  Widget _buildDatePicker(TextEditingController controller, String label) {
+    return TextField(
+      controller: controller,
+      readOnly: true,
+      decoration: InputDecoration(
+        labelText: label,
+        border: OutlineInputBorder(),
+      ),
+      onTap: () async {
+        DateTime? pickedDate = await showDatePicker(
+          context: context,
+          initialDate: DateTime.now(),
+          firstDate: DateTime(2000),
+          lastDate: DateTime(2101),
+        );
+        if (pickedDate != null) {
+          setState(() {
+            controller.text = DateFormat('yyyy-MM-dd').format(pickedDate);
+          });
+        }
       },
+    );
+  }
+
+  void _editData(Map<String, dynamic> item) {
+    setState(() {
+      _selectedItem = item;
+      _namaController.text = item['nama'];
+      _alamatController.text = item['alamat'];
+      _namaBukuController.text = item['nama_buku'];
+      _tanggalSewaController.text = item['tanggal_sewa'];
+      _tanggalKembaliController.text = item['tanggal_kembali'];
+    });
+  }
+
+  void _deleteData(int id) async {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Hapus data'),
+          content: Text('Apakah Anda yakin ingin menghapus data ini?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Batal'),
+            ),
+            TextButton(
+              onPressed: () async {
+                await dbHelper.deleteSewa(id);
+                _loadNotes();
+                Navigator.of(context).pop();
+                _showSnackbar("Data berhasil dihapus!");
+              },
+              child: Text('Hapus'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 66, 31, 18),
+        title: Text(
+          'Sewa Buku',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            // Form Input di Atas
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  _buildTextField(_namaController, 'Nama'),
+                  SizedBox(height: 10),
+                  _buildTextField(_alamatController, 'Alamat'),
+                  SizedBox(height: 10),
+                  _buildTextField(_namaBukuController, 'Nama Buku'),
+                  SizedBox(height: 10),
+                  _buildDatePicker(_tanggalSewaController, 'Tanggal Sewa'),
+                  SizedBox(height: 10),
+                  _buildDatePicker(
+                      _tanggalKembaliController, 'Tanggal Pengembalian'),
+                  SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: _saveData,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(255, 66, 31, 18),
+                    ),
+                    child: Text(
+                      'Submit',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Divider(),
+            // List Data di Bawah
+            ListView.builder(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              itemCount: _notes.length,
+              itemBuilder: (context, index) {
+                final item = _notes[index];
+                return ListTile(
+                  title: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start, // Rata kiri
+                    mainAxisSize:
+                        MainAxisSize.min, // Menghindari ruang kosong ekstra
+                    children: [
+                      Text(
+                        item['nama'],
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      SizedBox(height: 4),
+                      Text(
+                        item['nama_buku'],
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.grey[600],
+                        ),
+                      ),
+                    ],
+                  ),
+                  subtitle: Text("Total Bayar: Rp ${item['total_bayar']}"),
+                  onTap: () => _editData(item),
+                  onLongPress: () => _deleteData(item['id']),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
